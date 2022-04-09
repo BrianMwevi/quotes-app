0af391745732735c090ef6687efc485e656c369f
brianmwevi <mwevibrian@gmail.com>
2022-04-09 21:30:18 +0300

    ch: app-modules -- add app libraries and modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'flash-messages-angular';
+
+import { UsersService } from './services/users.service';
+
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AccountComponent } from './components/account/account.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
+import { QuoteDetailComponent } from './components/quote-detail/quote-detail.component';
+import { AddQuoteComponent } from './components/add-quote/add-quote.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    AccountComponent,
+    ProfileComponent,
+    QuotesComponent,
+    QuoteDetailComponent,
+    AddQuoteComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    FlashMessagesModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase, 'clientpanel'),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+  ],
+  providers: [UsersService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
