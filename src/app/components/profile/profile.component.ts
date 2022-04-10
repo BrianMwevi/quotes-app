6177c56d3827349bc62ccd3550901981b051b75f
brianmwevi <mwevibrian@gmail.com>
2022-04-10 03:41:02 +0300

    ft: profile -- add profile page structure and functionality

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UsersService } from 'src/app/services/users.service';
+import { QuotesService } from 'src/app/services/quotes.service';
+import { subscribeOn } from 'rxjs';
+
+import { Quote } from '../../models/Quote';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+  userQuotes: any = [];
+  user: any;
 
-  constructor() { }
+  constructor(
+    private userService: UsersService,
+    private quoteService: QuotesService
+  ) {}
 
   ngOnInit(): void {
+    this.userService.afAuth.authState.subscribe((user) => (this.user = user));
+    this.quoteService
+      .getQuotes()
+      .subscribe(
+        (quotes) =>
+          (this.userQuotes = quotes.filter(
+            (quote) => quote.author.id === this.user?.uid
+          ))
+      );
   }
-
 }
